ef85cb2d9d758b80b3ac69e3ab1b254a7c92b5ea
Morrison Idiasirue <midiasirue@mgic-nigeria.org>
2020-10-22 08:56:06 +0100

    unused imports

--- src/main/java/com/nmrs/umb/biometriclinux/dal/DbManager.java
+++ src/main/java/com/nmrs/umb/biometriclinux/dal/DbManager.java
@@ -21,9 +21,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import javax.annotation.Resource;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 
 /**
  *
